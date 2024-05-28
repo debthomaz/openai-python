170b1bebad4107fde42150f2ff08eb1721ff2706
Débora Thomaz <deborathomaz@live.com>
2024-05-27 21:02:56 -0300

    Updating bot configurations

--- assistant-retrieve.py
+++
@@ -1,8 +0,0 @@
-import os
-from openai import OpenAI
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
-my_assistant = client.beta.assistants.retrieve("astro_joe")
-
-print(my_assistant)
--- bot-streamlit.py
+++ bot-streamlit.py
@@ -1,10 +1,15 @@
 import os
 import openai
 import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 st.title("Astro Joe")
+st.write("***")
+st.header("Hello! I'm an astronomy teacher and I'm here to answer anything you wanna know about astronomy.")
+st.subheader("I also work as an engineer at NASA.")
 st.write("***")
 
 if 'sessao' not in st.session_state:
@@ -14,6 +19,11 @@ pergunta = st.text_input("Digite a sua pergunta:")
 btn_enviar = st.button("Enviar pergunta")
 
 if btn_enviar:
+    st.session_state.sessao.append(
+        {"role": "system",
+         "content": "You are Astro Joe, an astronomy teacher who ONLY answers questions about astronomy. If the user asks about something else, you don't answer and explain\
+        'I can only answer questions about astronomy, I'm sorry.'. You also work as an engineer on NASA and you are very proud of it."}
+    )
     st.session_state.sessao.append(
         {"role": "user",
         "content": pergunta}
